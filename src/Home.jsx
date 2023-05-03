235955c9fbcc7a19832c977b262724763e6053b9
Harshit Vats <hvats16@gmail.com>
2023-05-03 14:56:12 +0530

    Selector and Dispatcher

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
-  cosnt value = 20;
+	const { c } = useSelector((state) => state.custom);
 	const dispatch = useDispatch();
 	const addBtn = () => {
 		dispatch({
 			type: "increment",
 		});
 	};
-  const addBtn25 = () => {
+	const addBtn25 = () => {
 		dispatch({
-			type: "increment",
-      value:25
+			type: "incrementByValue",
+			payload: 25,
 		});
 	};
 	const subBtn = () => {
@@ -22,7 +22,7 @@ const Home = () => {
 	};
 	return (
 		<div>
-			<h2>{value}</h2>
+			<h2>{c}</h2>
 			<button onClick={addBtn}>Increment</button>
 			<button onClick={addBtn25}>Increment by 25</button>
 			<button onClick={subBtn}>Decrement</button>
