59d93f9f305a42c9efc31335264d9ac0c6b93488
Harshit Vats <hvats16@gmail.com>
2023-05-03 14:51:14 +0530

    Dispatcher

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useDispatch } from "react-redux";
 
 const Home = () => {
-  const value = 20;
-  return (
-    <div>
-      <h2>{value}</h2>
-      <button>Increment</button>
-      <button>Decrement</button>
-    </div>
-  )
-}
+  cosnt value = 20;
+	const dispatch = useDispatch();
+	const addBtn = () => {
+		dispatch({
+			type: "increment",
+		});
+	};
+  const addBtn25 = () => {
+		dispatch({
+			type: "increment",
+      value:25
+		});
+	};
+	const subBtn = () => {
+		dispatch({
+			type: "decrement",
+		});
+	};
+	return (
+		<div>
+			<h2>{value}</h2>
+			<button onClick={addBtn}>Increment</button>
+			<button onClick={addBtn25}>Increment by 25</button>
+			<button onClick={subBtn}>Decrement</button>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
